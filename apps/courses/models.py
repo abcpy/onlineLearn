1a52deebe8e5c4fe6d7afe433de732a23deabaf3
python <15000728721@163.com>
2017-10-30 23:20:33 -0700

    xadmin

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -17,6 +17,9 @@ class Course(models.Model):
     chick_nums=models.IntegerField(default=0,verbose_name=u'点击数')
     add_time=models.DateTimeField(default=datetime.now,verbose_name=u'添加时间')
 
+    def __unicode__(self):
+        return self.name
+
     class Meta:
         verbose_name=u'课程'
         verbose_name_plural=verbose_name
--- apps/users/models.py
+++ apps/users/models.py
@@ -26,8 +26,11 @@ class EmailVerifyRecord(models.Model):
     """邮箱验证码"""
     code=models.CharField(max_length=20,verbose_name=u'验证码')
     email=models.EmailField(max_length=50,verbose_name=u'邮箱')
-    send_type=models.CharField(choices=(('register',u'注册'),('forget',u'未注册')),max_length=20)
-    send_time=models.DateTimeField(default=datetime.now)
+    send_type=models.CharField(choices=(('register',u'注册'),('forget',u'找回密码')),max_length=20,verbose_name=u'发送类型')
+    send_time=models.DateTimeField(default=datetime.now,verbose_name=u'发生时间')
+
+    def __unicode__(self):
+        return '{0}({1})'.format(self.code,self.email)
 
     class Meta:
         verbose_name=u'邮箱验证码'
